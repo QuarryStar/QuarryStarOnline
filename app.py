87f5a6bb006e49e9df196287e0e30f5143d597e3
lcoveney <lcoveney@iu.edu>
2025-09-11 13:40:04 -0400

    connection fix

--- app.py
+++ app.py
@@ -283,11 +283,11 @@ def logout():
 @app.route('/table/<table_name>')
 @login_required
 def view_table(table_name):
-    import os, sqlite3, traceback
+    
 
     DB_PATH = os.path.join(os.path.dirname(__file__), 'Databases', 'Bookings-FP.db')
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = open_conn()
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
 
